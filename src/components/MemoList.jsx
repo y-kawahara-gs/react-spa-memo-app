c4bf402eaafb02e584fba938d8db1cc3053bc1ca
y-kawahara-gs <y.kawahara@genbasupport.com>
2026-01-22 10:03:13 +0900

    style: MemoListのPrettierによる修正

--- src/components/MemoList.jsx
+++ src/components/MemoList.jsx
@@ -4,15 +4,13 @@ export default function MemoList({ memos, newMemoId, onClick }) {
       <ul>
         {memos.map((memo) => (
           <li key={memo.id}>
-          <button className="list-button" onClick={() => onClick(memo.id)}>
-            {memo.title}
-          </button>
+            <button className="list-button" onClick={() => onClick(memo.id)}>
+              {memo.title}
+            </button>
           </li>
         ))}
         <li>
-          <button onClick={() => onClick(newMemoId)}>
-            追加
-          </button>
+          <button onClick={() => onClick(newMemoId)}>追加</button>
         </li>
       </ul>
     </>
